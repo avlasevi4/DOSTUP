0cf1771a603fb5051a4e7afd9b385bf20f9137d8
avlasevi4 <avlasevi4@gmail.com>
2026-08-27 10:42:18 +0300

    Connect court date update worker

--- court-update-config.js
+++ court-update-config.js
@@ -1,4 +1,4 @@
 // Адрес Cloudflare Worker для проверки страниц судов.
 // После первой публикации обработчика замените пустую строку на адрес вида
 // https://dostup-court-updater.<ваш-аккаунт>.workers.dev
-export const COURT_UPDATE_ENDPOINT = '';
+export const COURT_UPDATE_ENDPOINT = 'https://dostup-court-updater.avlasevi4.workers.dev';
